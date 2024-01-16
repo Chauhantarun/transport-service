f9d3b1902718db12c88bd26c05236f805e4a69a7
Chauhantarun <153579089+Chauhantarun@users.noreply.github.com>
2024-01-16 12:16:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,24 @@ import mongoose from "mongoose";
 import Router from "./Routes/route.js";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
 dotenv.config();
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "./transport-frontend/build")));
+
 app.use("/api", Router);
+
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./transport-frontend/build/index.html"));
+});
 
 app.listen(process.env.PORT || 8081, () => {
   console.log("server is running");
@@ -22,3 +34,4 @@ mongoose
   .catch((error) => {
     console.log("error in connecting database" + error);
   });
+
